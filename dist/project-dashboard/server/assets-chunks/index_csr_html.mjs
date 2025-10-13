978b7c1aa6fa12d0c3466c64fdbb92f59fcbc476
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2025-10-13 12:43:22 +0000

    att

---
+++ dist/project-dashboard/server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>CodeSpark</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-KK73LTR2.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-KK73LTR2.css"></noscript></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-2ARJUJRA.js" type="module"></script></body>
+</html>
+`;
